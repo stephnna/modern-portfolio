fd4344becc56116bceccf6c017831b21539b479d
Stephen Ezea <stephennnabuike@gmail.com>
2023-08-16 13:19:38 +0100

    Created Nav, hero and about section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
     <nav
     className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
     >
-      <div className='w-full flex justify-between items-center max-w-7x1 mx-auto'>
+      <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
         <Link
         to="/"
         className="flex items-center gap-2"
@@ -47,14 +47,18 @@ const Navbar = () => {
           onClick={() => setToggle(!toggle)}
           />
           <div className={`${!toggle ? 'hidden' : 'flex' } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}>
-          <ul className='list-none hidden sm:flex flex-row gap-10'>
+          <ul className='list-none flex justify-end items-start flex-col gap-4'>
           {navLinks.map((link) => (
             <li
             key={link.id}
             className={`${
               active===link.title ? "text-white" : "text-secondary"
 
-            } hover:text-white text-[18px] font-medium`}
+            } font-poppins font-medium cursor-pointer text-[16px]`}
+            onClick={() => {
+              setToggle(!toggle)
+              setActive(link.title);
+            }}
             >
               <a href={`#${link.id}`}>{link.title}</a>
             </li>
