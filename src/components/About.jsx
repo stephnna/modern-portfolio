d0ef2143e1e784c4bca087cee4997e283e280471
Stephen Ezea <stephennnabuike@gmail.com>
2023-08-29 16:38:28 +0100

    Improve responsive design

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -48,17 +48,17 @@ const About = () => {
     <>
       <motion.div variants={textVariant()}>
         <p className={`ml-10 ${styles.sectionSubText}`}>Introduction</p>
-        <div className="flex justify-evenly">
+        <div className="flex justify-between flex-wrap">
         {Object.keys(services).map((heading) => (
           <h2
             key={heading}
-            className={`cursor-pointer ${
+            className={`cursor-pointer mx-10 ${
               activeHeading === heading ? 'bg-blue-500 rounded-xl px-2 text-white' : ''
             } ${styles.sectionHeadMany}`}
             onClick={() => handleClick(heading)}         
         
           >
-             {heading}
+             {heading}.
           </h2>
         ))} 
         </div>
@@ -66,13 +66,13 @@ const About = () => {
 
       <motion.div
         variants={fadeIn("", "", 0.1, 1)}
-        className='mt-4 mx-20 text-secondary text-[17px] leading-[30px]'
+        className='mt-4 mx-10 text-secondary text-[17px]]'
       >   
-          <p className="ml-8">{services[activeHeading].intro}</p>       
+          <p className="">{services[activeHeading].intro}</p>       
         
       </motion.div>
 
-      <div className='mt-20 flex flex-wrap justify-evenly mx-20 gap-2'>
+      <div className='mt-10 flex flex-wrap justify-evenly mx-20'>
         {services[activeHeading].content.map((item, index) => (
           <ServiceCard key={item.title} index={index}
           title={item.title} icon={item.icon} description={item.description} />
